4fba6e990660a51dc723cb0bec213a0b539e2f92
mcdonalddylan <soupaface@gmail.com>
2022-06-09 00:07:03 -0400

    Updates

---
+++ precache-manifest.2748ea003650f369b4a706f0932d425f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "862af503af5fd352a376",
+    "url": "/AvoidTheSquares/static/css/main.9103733e.chunk.css"
+  },
+  {
+    "revision": "862af503af5fd352a376",
+    "url": "/AvoidTheSquares/static/js/main.862af503.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/AvoidTheSquares/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "5f6b26b863b7ddbec118",
+    "url": "/AvoidTheSquares/static/js/2.5f6b26b8.chunk.js"
+  },
+  {
+    "revision": "ba8a0bf64da7cbe8b58d",
+    "url": "/AvoidTheSquares/static/js/runtime~main.ba8a0bf6.js"
+  },
+  {
+    "revision": "be87adf8b12f15fde28b89112923bcbd",
+    "url": "/AvoidTheSquares/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.eefcfa973ad9a200b9f6eeaea03e2ce2.js"
+  "/AvoidTheSquares/precache-manifest.2748ea003650f369b4a706f0932d425f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/AvoidTheSquares/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
